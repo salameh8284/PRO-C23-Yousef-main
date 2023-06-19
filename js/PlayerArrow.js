30fe8bbdc7e91ebad9add5aef73045c99d6aa506
salameh8284 <126583912+salameh8284@users.noreply.github.com>
2023-06-19 21:36:36 +0400

    commit

--- js/PlayerArrow.js
+++ js/PlayerArrow.js
@@ -10,15 +10,16 @@ class PlayerArrow {
     // this.body = Bodies.rectangle(x, y, this.width, this.height);
     // this.body = Body.rectangle(x, y, this.width, this.height, options);
     // this.body = Bodies.rectangle(x, y, options);
-    // this.body = Bodies.rectangle(x, y, this.width, this.height, options);
+    this.body = Bodies.rectangle(x, y, this.width, this.height, options);
     
     this.image = loadImage("./assets/arrow.png");
     World.add(world, this.body);
   }
 
   shoot(archerAngle) {
-    var velocity = p5.Vector.fromAngle(archerAngle);
-    velocity.mult(20);
+    console.log(archerAngle);
+    var velocity = p5.Vector.fromAngle(archerAngle); //should be tuned!!
+    velocity.mult(5);
     Matter.Body.setStatic(this.body, false);
     Matter.Body.setVelocity(this.body, { x: velocity.x, y: velocity.y });
   }
